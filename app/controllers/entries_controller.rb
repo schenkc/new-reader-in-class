d163c4ac5a9e7dbba9a965b48132fc1634028f35
candace & jorge <candace&jorge@appacademy.io>
2014-04-16 09:09:01 -0400

    init commit

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,12 @@
+class EntriesController < ApplicationController
+  def index
+    feed = Feed.find(params[:feed_id])
+    render :json => feed.entries
+  end
+
+  private
+  def entry_params
+    params.require(:entry)
+      .permit(:guid, :link, :published_at, :title, :json, :feed_id)
+  end
+end
---
+++ app/controllers/feeds_controller.rb
@@ -0,0 +1,22 @@
+class FeedsController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render :json => Feed.all }
+    end
+  end
+
+  def create
+    feed = Feed.find_or_create_by_url(feed_params[:url])
+    if feed
+      render :json => feed
+    else
+      render :json => { error: "invalid url" }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def feed_params
+    params.require(:feed).permit(:title, :url)
+  end
+end
